c349f4ec96d15060a67c65af5e2ef040874832bb
Visar Domi <visardomi4@gmail.com>
2019-03-10 03:02:45 +0100

    google font is served over https

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div class="nav-on-header smart-nav" id="body">
-      <CompHeader />
+      <CompHeader/>
       <router-view></router-view>
-      <CompFooter />
+      <CompFooter/>
       <!-- Back to top button -->
       <a id="scroll-up" href="#">
         <i class="ti-angle-up"></i>
@@ -17,18 +17,18 @@ import CompHeader from "@/components/TheHeader";
 import CompFooter from "@/components/TheFooter";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     CompHeader,
     CompFooter
   }
-}
+};
 </script>
 
 <style>
 @import "../public/static/css/app.min.css";
 @import "../public/static/css/custom.css";
-@import url("http://fonts.googleapis.com/css?family=Oswald:100,300,400,500,600,800%7COpen+Sans:300,400,500,600,700,800%7CMontserrat:400,700");
+@import url("https://fonts.googleapis.com/css?family=Oswald:100,300,400,500,600,800%7COpen+Sans:300,400,500,600,700,800%7CMontserrat:400,700");
 @import url();
 .navbar .container {
   width: 100%;
